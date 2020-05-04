41c9cfbece602d7213f080dc072c848af890cb96
kraevskiy <ilya.labelplatform@gmail.com>
2020-05-04 12:29:35 +0300

    conected to MongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+module.exports = {
+  MONGO_URI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const app = require('./app')
+const mongoose = require('mongoose')
 const port = process.env.PORT || 5000
+const keys = require('./config/keys')
 
-app.listen(port, () => {
-  console.log(`Server has been started on ${port}`);
-})
+async function start(){
+  try {
+    mongoose.connect(keys.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+      .then(() => console.log('MongoDB connected.'))
+      .catch(e => console.log('Error connected to MongoDB', e))
+
+    app.listen(port, () => {
+      console.log(`Server has been started on ${port}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
