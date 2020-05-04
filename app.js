f185632ea9541328c29d55f1ff30262971b7cd56
kraevskiy <ilya.labelplatform@gmail.com>
2020-05-04 20:36:26 +0300

    finished create,update controllers/category && set static directory

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(passport.initialize())
 require('./middleware/passport')(passport)
 
 app.use(require('morgan')('dev'))
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(require('cors')())
--- controllers/category.js
+++ controllers/category.js
@@ -33,16 +33,33 @@ module.exports.remove = async (req, res) => {
 }
 
 module.exports.create = async (req, res) => {
+  const category = new Category({
+    name: req.body.name,
+    user: req.user.id,
+    imageSrc: req.file ? req.file.path : ''
+  })
   try {
-
+    await category.save()
+    res.status(201).json(category)
   } catch (e) {
     errorHandler(res, e)
   }
 }
 
 module.exports.update = async (req, res) => {
+  const updated = {
+    name: req.body.name
+  }
+  if (req.file) {
+    updated.imageSrc = req.file.path
+  }
   try {
-
+    const category = await Category.findOneAndUpdate(
+      {_id: req.params.id},
+      {$set: updated},
+      {new: true}
+    )
+    res.status(200).json(category)
   } catch (e) {
     errorHandler(res, e)
   }
