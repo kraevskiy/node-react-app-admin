b39863022abeb54b5314949d6cce152ac5d3ad01
kraevskiy <ilya.labelplatform@gmail.com>
2020-05-04 10:35:44 +0300

    create routes and controller from API login/register

---
+++ controllers/auth.js
@@ -0,0 +1,11 @@
+module.exports.login = function (req, res) {
+  res.status(200).json({
+    loginFromController: true
+  })
+}
+
+module.exports.register = function (req, res) {
+  res.status(200).json({
+    loginFromController: true
+  })
+}
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const controller = require('../controllers/auth')
+const router = express.Router()
+
+router.get('/login', controller.login)
+
+router.get('/register', controller.register)
+
+
+module.exports = router
